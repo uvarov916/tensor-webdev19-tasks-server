9343dcfe24e7f63510c65dfef68fd2428790383c
Ivan Uvarov <is.uvarov@tensor.ru>
2019-05-13 02:01:34 +0300

    Changed cookies for heroku

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(cookieParser());
+app.use(cookieParser('secrectcode'));
 
 app.use(user.authMiddleware);
 
