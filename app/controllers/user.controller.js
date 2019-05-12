556abd69fbef0a5489cc083ebd593cb7200846c8
Ivan Uvarov <is.uvarov@tensor.ru>
2019-05-13 02:04:40 +0300

    Changed cookies for heroku

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -70,7 +70,7 @@ exports.login = (req, res) => {
                 } else if (result) {
                     SessionController.create(username)
                         .then(createdSessionId => {
-                            res.cookie('sid', createdSessionId, {path: '/'});
+                            res.cookie('sid', createdSessionId);
                             res.send(createdSessionId);
                         })
                         .catch(err => {
